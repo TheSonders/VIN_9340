b9c81f5b71fe2d4c478d5cfb5080cd7457ffd93a
Antonio Sanchez <75912990+TheSonders@users.noreply.github.com>
2021-06-13 00:32:00 +0200

    Add files via upload

--- FPGA_DESIGN/GEN_9341.v
+++ FPGA_DESIGN/GEN_9341.v
@@ -16,27 +16,27 @@ module GEN_9341(
     //CPU interface
     inout wire    [7:0]d,
     input wire         e,
-    input wire       _cs,
+    input wire      cs_n,
     input wire       r_w,
     input wire       b_a,
     input wire       c_t,
     //VIN interface
-    output wire       _ve,
+    output wire      ve_n,
     inout wire  [7:0]busA,
     inout wire  [7:0]busB,
     input wire       r_wi,
-    input wire        _sm,
-    input wire        _st,
-    input wire        _sg,
+    input wire       sm_n,
+    input wire       st_n,
+    input wire       sg_n,
     input wire   [3:0]adr);
     
-assign d=(e & ~_cs & r_w)?(c_t)?{Busy,6'h00}:(b_a)?TB:TA:8'hZZ; //Top of page 13
-assign busA=(r_w & ~_sg & (Gen_Selected || Del_Selected))?OutA: //Cycle TYPE 2,6
-            (~r_w & ~_st)? TA:                          //Cycle TYPE 3,5,7
+assign d=(e & ~cs_n & r_w)?(c_t)?{Busy,6'h00}:(b_a)?TB:TA:8'hZZ; //Top of page 13
+assign busA=(r_w & ~sg_n & (Gen_Selected || Del_Selected))?OutA: //Cycle TYPE 2,6
+            (~r_w & ~st_n)? TA:                          //Cycle TYPE 3,5,7
             8'hZZ; //Schematics on page 2
-assign busB=(~r_w & ~_st)? TB:                          //Cycle TYPE 3,5,7
+assign busB=(~r_w & ~st_n)? TB:                          //Cycle TYPE 3,5,7
             8'hZZ;
-assign _ve=~Busy;
+assign ve_n=~Busy;
 
 //Accesible registers
 reg [7:0] TA=0;     //This both are the mailbox
@@ -53,8 +53,8 @@ reg m=0;
 reg prev_e=1;       //Rise detection of input e
 reg prev_sm=0;      //Fall detection of signal from VIN
 reg prev_sg=0;      
-reg pprev_sm=0;     //The signal _st may be delayed (tAS on Page 7)...
-reg pprev_sg=0;     //Use of double latch to detect delayed _st     
+reg pprev_sm=0;     //The signal st_n may be delayed (tAS on Page 7)...
+reg pprev_sg=0;     //Use of double latch to detect delayed st_n     
 reg prev_st=0;
 reg pprev_st=0;
 
@@ -65,11 +65,11 @@ initial $readmemh ("charset_ef9341.txt",ROM);
 
 always @(posedge clk)begin
     prev_e<=e;
-    prev_sm<=_sm;pprev_sm<=prev_sm;
-    prev_sg<=_sg;pprev_sg<=prev_sg;
-    prev_st<=_st;pprev_st<=prev_st;
+    prev_sm<=sm_n;pprev_sm<=prev_sm;
+    prev_sg<=sg_n;pprev_sg<=prev_sg;
+    prev_st<=st_n;pprev_st<=prev_st;
     //CPU ACCESS
-    if (~prev_e & e & ~_cs) begin          
+    if (~prev_e & e & ~cs_n) begin          
         case ({c_t,b_a,r_w})               //Table on top of page 13
             3'b000:begin TA<=d;end         //Reading accesses are multiplexed on assign
             3'b010:begin TB<=d;Busy<=1;end
@@ -79,9 +79,9 @@ always @(posedge clk)begin
         endcase
     end
     //INTERNAL BUS ACCESS
-    if (pprev_sm & ~prev_sm & ~_sm)begin   //Delayed low pulse on _sm
+    if (pprev_sm & ~prev_sm & ~sm_n)begin   //Delayed low pulse on sm_n
         if (r_wi) begin
-            if (_st)begin                   //Cycle TYPE 1 
+            if (st_n)begin                   //Cycle TYPE 1 
                 Gen_Selected<=(~busB[7]);
                 CC<={busA[7],busB[6:0]}*10;
                 if ({busB[7:5],busA[7]}==4'b1000) begin //Delimitor
@@ -98,9 +98,9 @@ always @(posedge clk)begin
             end
         end
     end
-    else if (pprev_sg & ~prev_sg & ~_sg)begin   //Delayed low pulse on _sg
+    else if (pprev_sg & ~prev_sg & ~sg_n)begin   //Delayed low pulse on sg_n
         if (r_wi) begin
-            if (_st)begin                   //Cycle TYPE 2 
+            if (st_n)begin                   //Cycle TYPE 2 
                 if (Gen_Selected) OutA<=ROM[CC+adr];
                 else if (Del_Selected) OutA<={5'h00,s,i,m};
             end
@@ -110,6 +110,6 @@ always @(posedge clk)begin
             end
         end
     end
-    else if (pprev_st & ~prev_st & ~_st & ~r_w) Busy<=0; //Cycle TYPE 3
+    else if (pprev_st & ~prev_st & ~st_n & ~r_w) Busy<=0; //Cycle TYPE 3
 end   
 endmodule
